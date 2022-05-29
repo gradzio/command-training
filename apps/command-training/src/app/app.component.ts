912a8573e0a7ed5f93de27470ca94933b5535c48
gradzio <gregradzio@gmail.com>
2022-05-29 09:13:30 +0200

    initial commit

---
+++ apps/command-training/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'command-training-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'command-training';
+}
